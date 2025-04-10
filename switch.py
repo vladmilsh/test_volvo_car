8dbaad6bc160152ff2d75a189b3cdfcc83bf48b7
vladmilsh <92649822+vladmilsh@users.noreply.github.com>
2025-04-11 00:27:45 +0300

    Add files via upload

--- switch.py
+++ switch.py
@@ -6,3 +6,30 @@ from . import vmc
 from .const import DOMAIN
 import logging
 _LOGGER = logging.getLogger(__name__)
+
+class MySwitch(SwitchEntity):
+    # Implement one of these methods.
+
+    def turn_on(self, **kwargs) -> None:
+        """Turn the entity on."""
+
+    async def async_turn_on(self, **kwargs):
+        """Turn the entity on."""
+
+class MySwitch(SwitchEntity):
+    # Implement one of these methods.
+
+    def turn_off(self, **kwargs):
+        """Turn the entity off."""
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the entity off."""
+
+class MySwitch(SwitchEntity):
+    # Implement one of these methods.
+
+    def toggle(self, **kwargs):
+        """Toggle the entity."""
+
+    async def async_toggle(self, **kwargs):
+        """Toggle the entity."""
